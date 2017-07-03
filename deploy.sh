223e85feeae57d8fbabe68e23aa22e28fd1adf27
scrpgil <scraplbuild@gmail.com>
2017-07-04 01:39:00 +0900

    [add]PWAデプロイのためのスクリプトファイル

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+ionic build browser --prod
+cd platforms/browser/www
+git init
+git add . 
+git commit -m "auto deploy" 
+git branch gh-pages 
+git checkout gh-pages 
+git remote add origin https://github.com/scrpgil/xpresenter.git
+git push -f origin gh-pages
